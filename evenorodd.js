effdfe1ebcb5a99403b31d171ce59ba07896946d
arifsyahrudin <arifsyah1018@gmail.com>
2024-01-26 19:17:38 +0700

    practice 1

---
+++ evenorodd.js
@@ -0,0 +1,19 @@
+// Practise Codewars - Even or Odd
+// Difficuilt Easiest
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/solutions/javascript?filter=me&sort=oldest&invalids=false
+
+
+// option 1 
+/*function evenOrOdd(number) {
+    if(number % 2 === 0) {
+        return 'Even'
+    } else {
+        return 'Odd'
+    }
+}
+console.log(evenOrOdd(0)) */
+
+
+// option 2
+const evenOrOdd = (number) => number % 2 === 0 ? "Even" : "Odd";
+console.log(evenOrOdd(0))
