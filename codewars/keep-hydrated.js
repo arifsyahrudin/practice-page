db17175d45963124e7f389c5ba4a80919d0fddcf
arifsyahrudin <arifsyah1018@gmail.com>
2024-01-28 20:55:20 +0700

    add new practice

---
+++ codewars/keep-hydrated.js
@@ -0,0 +1,6 @@
+// Practice Codewars - Keep Hydrated
+// Difficuilt Easiest
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/javascript
+
+const litres = (time) => Math.floor(time * 0.5);
+console.log(litres(2));
